b6d688cd7d3ef122822419551aba369acb0fe632
barbaraborges <basborges@gmail.com>
2018-12-09 10:52:58 -0200

    Adding menu

---
+++ src/components/Header.js
@@ -0,0 +1,21 @@
+import React, {Component} from 'react';
+import '../css/Header.css'
+
+class Header extends Component {
+    render() {
+        return(
+            <header className="header__container">
+            <nav>
+                <ul className="navBar__menu">
+                    <li className="navBar__menuItem greetings">Hello, {this.props.username}!</li>
+                    <li className="navBar__menuItem"><a href="/">Home</a></li>
+                    <li className="navBar__menuItem"><a href="">Login</a></li>
+                </ul>
+            </nav>
+            </header>
+        );
+    }
+    
+} 
+
+export default Header;
