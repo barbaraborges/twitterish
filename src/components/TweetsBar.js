acb9ef39e82e8011fc487e8afa86ba851722773a
barbaraborges <basborges@gmail.com>
2018-12-17 10:16:38 -0200

    Creates a tweet list and displays a message if there are no tweets yet

--- src/components/TweetsBar.js
+++ src/components/TweetsBar.js
@@ -7,26 +7,71 @@ class TweetsBar extends Component {
     constructor() {
         super();
         this.state = {
+            newTweet: '',
             oldTweets: []
+        }
+
+        this.addTweet = this.addTweet.bind(this);
+        this.renderTweets = this.renderTweets.bind(this);
+    }
+
+    addTweet(event) {
+        event.preventDefault();
+
+        const newTweet = this.state.newTweet;
+        const oldTweets = this.state.oldTweets;
+
+        if(newTweet) {
+            this.setState({
+                oldTweets: [ newTweet, ...oldTweets ],
+                newTweet: ''
+            });
         }
     }
 
+    renderTweets = (tweet, index) => { return ( <Fragment key={ index }>
+                                                    <hr/>
+                                                    <Tweet content={ tweet }  />
+                                                </Fragment>
+                                                ) };
+
     render(){
+
+        let tweetsList;
+
+        if (this.state.oldTweets.length > 0) { 
+            tweetsList = this.state.oldTweets.map(
+                this.renderTweets
+            ); 
+        }
+
+        else tweetsList = ( <div className="tweet__container">
+                                <div className="tweet__info">No tweets yet!</div>
+                            </div>);
+
         return(
             <Fragment>
                 <div className="tweetsBar__container contentBox">
                     <h1 className="tweetsBar__header">What's up?</h1>
                     <hr/>
-                    <NewTweet oldTweets={this.state.oldTweets } />
+                    
+                    {/* Equivalent to NewTweet */}
 
-                    { this.state.oldTweets.map(
-                        tweet => { return (
-                            <Fragment>
-                                <hr/>
-                                <Tweet content={ tweet }/>
-                            </Fragment>
-                        ) }
-                    ) }
+                    <span className={`tweet__charCounter ${ this.state.newTweet.length > 140 ? 'tweet__charCounter--invalid' : ''}`}>{ this.state.newTweet.length }/140 characters</span>
+                    <form type="POST" onSubmit={ this.addTweet }>
+                        <textarea className="tweet__input" rows="4" 
+                            placeholder="Spread the word!"
+                            value={ this.state.newTweet } 
+                            onChange={ (event) => this.setState({ newTweet: event.target.value }) }
+                            onInput={ (event) => this.setState({ newTweet: event.target.value })}/>
+                        <button className="tweet__button"
+                                disabled={ this.state.newTweet.length > 140 }
+                                type="submit">Post</button>
+                    </form>
+
+                    {/* End equivalence to NewTweet */}
+                    
+                    { tweetsList }
             
                 </div>
             </Fragment>
